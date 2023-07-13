4cfefa1fa8a22af39d62cb37164460ae8730aa07
nguyendangduy <nguyendangduy269@gmail.com>
2023-07-13 16:45:16 +0700

    chore: add dating page

--- components/DateCard.vue
+++ components/DateCard.vue
@@ -1,16 +1,18 @@
 <template>
-  <NuxtLink :to="`/date/${date}`">
-    <div class="flex items-center">
+  <div class="flex items-center">
+    <NuxtLink :to="`/date/${date}`" class="flex items-center">
       <img src="/calendar.png" alt="calendar" width="25" class="mr-2" />
       <p class="font-medium">{{ dayjs(date).format("DD/MM/YYYY") }}</p>
+    </NuxtLink>
+    <NuxtLink to="/dating">
       <svg v-if="dating" class="heart ml-2" viewBox="0 0 32 29.6">
         <path
           d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2
               c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"
         />
       </svg>
-    </div>
-  </NuxtLink>
+    </NuxtLink>
+  </div>
 </template>
 
 <script lang="ts" setup>
