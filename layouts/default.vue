1f49428de409a3d0fc00b0bd43f3813b059054ea
Duy Nguyễn <nguyendangduy269@gmail.com>
2023-07-23 23:21:45 +0700

    chore: add password

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,19 @@
 <template>
-  <TheHeader/>
-  <slot/>
+  <template v-if="confirm === '26112609'">
+    <TheHeader />
+    <slot />
+  </template>
 </template>
+
+<script lang="ts" setup>
+const confirm: any = ref([]);
+
+onMounted(() => {
+  let password = prompt("Nhập mật khẩu");
+  confirm.value = password;
+
+  if (confirm.value !== "26112609") {
+    location.reload();
+  }
+});
+</script>
